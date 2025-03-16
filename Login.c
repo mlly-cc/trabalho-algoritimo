03b51772b26015fbad01e21e7727f3acbc8998fb
mlly-cc <eva@aluno.ifnmg.edu.br>
2025-03-15 23:22:59 -0300

    Update Login.c

--- Login.c
+++ Login.c
@@ -47,4 +47,57 @@ int VerificarLogin() {
     }
 
                   //  rewind(file); // Reinicia a leitura do arquivo para nova tentativa
+void menu2(){
+    int opcao2;
+    int opcao3;        
+    int opcao4;
+    int opcao5;
+    printf("Digite (1) paciente \n (2) medico \n (3) consulta \n (0) para encerrar)...");
+    scanf("%d%*c", &opcao2);
+    if (opcao2 == 1) {
+        printf("Digite (1) para cadastrar novo paciente \n (2) buscar paciente(nome) e exibir \n (3) alterar dados do paciente(exceto o CPF)\n (0) para encerrar)...");
+        scanf("%d%*c", &opcao3);
+        if (opcao3 == 1) {
+            InserirNovoPaciente();
+        }else if (opcao3 == 2) {
+            BuscarPacientePorNome(); 
+            } else if (opcao3 == 3) {
+                AlterarDadosPaciente(); 
+                } else if (opcao3 ==0)
+                    exit(0);   
+   }
+
+   else if (opcao2 == 2) {
+        printf("Digite (1) para cadastrar novo medico \n (2) buscar medico(nome) e exibir \n (3) listar medicos de x especialidade \n (4) alterar dados do medico(exceto o CRM)\n (0) para encerrar)...");
+        scanf("%d%*c", &opcao4);
+        if (opcao4== 1) {
+            InserirNovoMedico();
+        }else if (opcao4 == 2) {
+            BuscarMedicoPorNome(); 
+            } else if (opcao4 == 3) {
+                ListarMedicosPorEspecialidade(); 
+                } else if (opcao4 == 4) {
+                    AlterarDadosMedico(); 
+                    } else if (opcao4 ==0)
+                        exit(0);   
+        }
+
+        else if (opcao2 == 3) {
+        printf("Digite (1) para cadastrar uma nova consulta \n (2) listar todas consultas de um medico \n (3) listar consultas de um paciente \n (4) listar consultas de um dia\n (0) para encerrar)...");
+        scanf("%d%*c", &opcao5);
+        if (opcao5== 1) {
+            InserirNovaConsulta();
+        }else if (opcao5 == 2) {
+            ListarConsultasPorMedico(); 
+            } else if (opcao5 == 3) {
+                ListarConsultasPorPaciente(); 
+                } else if (opcao5 == 4) {
+                    ListarConsultasPorData(); 
+                    } else if (opcao5 ==3)
+                        exit(0);   
+        }  
+// system ("pause");//pausa na tela, somente para Windows
+}
+
+
 
